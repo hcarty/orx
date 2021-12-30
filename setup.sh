e25c4448361e70e29be5ac942f83dc4d6483a1a3
Hezekiah M. Carty <hez@0ok.org>
2021-12-29 23:09:06 -0700

    Force a specific rebol version

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@ if [ "${OS}" = "Darwin" ]; then
 else
     if [ "${ARCH}" = "x86_64" ]; then
         echo "I am 64 bit"
-        REBOL=code/build/rebol/r3-linux
+        REBOL=code/build/rebol/r3-linux64
     else
         echo "I am 32 bit"
         REBOL=code/build/rebol/r3-linux
     fi
 fi
 
-${REBOL} -qs code/build/rebol/setup.r
+code/build/rebol/r3-linux64 -qs code/build/rebol/setup.r
