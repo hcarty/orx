2c15e754601b2213c17c4ea105ba036793ee980a
Hezekiah M. Carty <hez@0ok.org>
2022-01-29 08:55:08 -0700

    Undo previous changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,10 @@ if [ "${OS}" = "Darwin" ]; then
     REBOL=code/build/rebol/r3-mac
 else
     if [ "${ARCH}" = "x86_64" ]; then
-        echo "I am 64 bit"
         REBOL=code/build/rebol/r3-linux64
     else
-        echo "I am 32 bit"
         REBOL=code/build/rebol/r3-linux
     fi
 fi
 
-code/build/rebol/r3-linux64 -qs code/build/rebol/setup.r
+${REBOL} -qs code/build/rebol/setup.r "$@"
